0353d9d9cce4515d12b89442a6f85c24bbaca6ee
currone <94299594+snackmasta@users.noreply.github.com>
2024-01-20 11:09:14 +0700

    Create digi.ino

---
+++ digi.ino
@@ -0,0 +1,24 @@
+/*
+  Following payload will grab saved Wifi password and will send them to your hosted webhook.
+*/
+
+#include "DigiKeyboard.h"
+
+void setup() {
+  pinMode(1, OUTPUT); //LED on Model A 
+}
+
+void loop() {
+  digitalWrite(1, HIGH); //turn on led when program finishes
+  DigiKeyboard.delay(1000);
+  digitalWrite(1, LOW); 
+  DigiKeyboard.delay(1000);
+  digitalWrite(1, HIGH); //turn on led when program finishes
+  DigiKeyboard.delay(1000);
+  digitalWrite(1, LOW); 
+  DigiKeyboard.delay(1000);
+  digitalWrite(1, HIGH); //turn on led when program finishes
+  DigiKeyboard.delay(1000);
+  digitalWrite(1, LOW); 
+  DigiKeyboard.delay(1000);
+}
